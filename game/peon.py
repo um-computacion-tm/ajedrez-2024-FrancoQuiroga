5e16d085c5f2eef36e666d923acf0d5fe98f2fd6
FrancoQuiroga <fe.quiroga@alumno.um.edu.ar>
2024-08-20 09:38:43 -0300

    Arregla el movimiento del peo

--- game/peon.py
+++ game/peon.py
@@ -5,3 +5,16 @@ class Peon(Piezas):
     def __init__(self, COLOR) -> None:
         super().__init__(COLOR)
         
+    def movimiento(self,desde_fila,desde_col,hasta_fila,hasta_col):
+        # Movimientos en posicion inicial
+        if (desde_fila == 1) and (self.__color__ == 'BLACK'):
+            if (hasta_fila == 2) or (hasta_fila == 3):
+                return True
+            
+        if (desde_fila == 6) and (self.__color__ == 'WHITE'):
+            if (hasta_fila == 5) or (hasta_fila == 4):
+                return True
+        else:
+            return False
+
+            
--- test/test_peon.py
+++ test/test_peon.py
@@ -2,11 +2,41 @@ from game.peon import Peon
 import unittest
 class Test_inicial_Peon(unittest.TestCase):
     def setUp(self) -> None:
-        self.peon_pruebablanco = Peon('WHITE')
-        self.peon_pruebanegro = Peon('BLACK')
+        self.peon_prueba_negroblanco = Peon('WHITE')
+        self.peon_prueba_negronegro = Peon('BLACK')
 
     def test_initpeon(self):
         pass
 
+
+class Test_movimiento_Peon(unittest.TestCase):
+    def setUp(self) -> None:
+        self.peon_prueba_blanco = Peon('WHITE')
+        self.peon_prueba_negro = Peon('BLACK')
+
+    def test_movim_posicion_inicial_blancoynegro(self):
+        #test que permite 
+        # que el peon se mueva 1 o 2 posiciones 
+        # en su primer turno
+        self.assertTrue(self.peon_prueba_negro.movimiento(1,0, 2,0))
+        self.assertTrue(self.peon_prueba_negro.movimiento(1,0, 3,0))
+
+        self.assertTrue(self.peon_prueba_blanco.movimiento(6,0, 5,0))
+        self.assertTrue(self.peon_prueba_blanco.movimiento(6,0, 4,0))
+    
+    def test_movi_posicion_inicial_novalida_blancoynegro(self):
+        self.assertFalse(self.peon_prueba_blanco.movimiento(5,0, 3,0))
+        self.assertFalse(self.peon_prueba_negro.movimiento(1,0, 4,0))
+        self.assertFalse(self.peon_prueba_blanco.movimiento(6,0, 3,0))
+        
+
+    def test_movim_peon_blanco_val(self):
+
+        self.assertTrue(self.peon_prueba_blanco.movimiento(6,0, 4,0))
+        
+
+    def test_movimiento(self):
+        
+        pass
 if __name__ == '__main__':
     unittest.main()
